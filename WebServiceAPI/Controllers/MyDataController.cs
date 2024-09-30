f2e43dd4861a56ae712205d6bf8e1110c2e48982
lokeshgithub590 <lokeshvemulapally123@gmail.com>
2024-09-30 19:07:14 +0530

    assessment 4.0

---
+++ WebServiceAPI/Controllers/MyDataController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebServiceAPI.Models;
+
+namespace WebServiceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyDataController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] MyDataModel data)
+        {
+            if (data == null)
+            {
+                return BadRequest("Invalid data.");
+            }
+            return Ok(new { Message = "Data received successfully!", Data = data });
+        }
+    }
+}
---
+++ WebServiceAPI/Models/MyDataModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebServiceAPI.Models
+{
+    public class MyDataModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
